12476c49256a3efac0d4ce1ccde5f4570d53a43f
John Oxley <john.oxley@liquidtelecom.com>
2015-03-18 15:37:09 +0000

    Removed unnecessary cisco_mibs

--- cisco_mibs/README.md
+++
@@ -1,4 +0,0 @@
-In order to build the CISCO-RTTMON-MIB, we need to download V2 from http://tools.cisco.com/Support/SNMP/do/BrowseMIB.do?local=en&mibName=CISCO-RTTMON-MIB
-
-Note that this has to be done on a linux box :(
-
--- cisco_mibs/buildmib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-for x in SNMPv2-SMI SNMPv2-TC IANAifType-MIB SNMPv2-CONF RFC1155-SMI RFC-1212 SNMPv2-MIB RFC1213-MIB IF-MIB RFC1271-MIB BRIDGE-MIB TOKEN-RING-RMON-MIB RMON-MIB RMON2-MIB SNMP-FRAMEWORK-MIB P-BRIDGE-MIB CISCO-SMI Q-BRIDGE-MIB CISCO-QOS-PIB-MIB RFC-1215 SNMPv2-TC-v1 CISCO-RTTMON-TC-MIB CISCO-ETHER-CFM-MIB CISCO-RTTMON-MIB ; do
-    echo ============== Working on $x  ==============
-    build-pysnmp-mib $x.my > $x.py
-    echo ============== Done ==============
-
-done
