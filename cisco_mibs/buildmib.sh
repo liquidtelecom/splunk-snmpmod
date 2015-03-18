0a7fadaecd47ee1f4ce5a77b4c01405c6ff246da
John Oxley <john.oxley@liquidtelecom.com>
2015-03-18 14:50:09 +0000

    Updated buildmibs

--- cisco_mibs/buildmib.sh
+++ cisco_mibs/buildmib.sh
@@ -1,26 +1,8 @@
 #!/bin/bash 
 
-build-pysnmp-mib SNMPv2-SMI.my > SNMPv2-SMI.py
-build-pysnmp-mib SNMPv2-TC.my > SNMPv2-TC.py
-build-pysnmp-mib IANAifType-MIB.my > IANAifType-MIB.py
-build-pysnmp-mib SNMPv2-CONF.my > SNMPv2-CONF.py
-build-pysnmp-mib RFC1155-SMI.my > RFC1155-SMI.py
-build-pysnmp-mib RFC-1212.my > RFC-1212.py
-build-pysnmp-mib SNMPv2-MIB.my > SNMPv2-MIB.py
-build-pysnmp-mib RFC1213-MIB.my > RFC1213-MIB.py
-build-pysnmp-mib IF-MIB.my > IF-MIB.py
-build-pysnmp-mib RFC1271-MIB.my > RFC1271-MIB.py
-build-pysnmp-mib BRIDGE-MIB.my > BRIDGE-MIB.py
-build-pysnmp-mib TOKEN-RING-RMON-MIB.my > TOKEN-RING-RMON-MIB.py
-build-pysnmp-mib RMON-MIB.my > RMON-MIB.py
-build-pysnmp-mib RMON2-MIB.my > RMON2-MIB.py
-build-pysnmp-mib SNMP-FRAMEWORK-MIB.my > SNMP-FRAMEWORK-MIB.py
-build-pysnmp-mib P-BRIDGE-MIB.my > P-BRIDGE-MIB.py
-build-pysnmp-mib CISCO-SMI.my > CISCO-SMI.py
-build-pysnmp-mib Q-BRIDGE-MIB.my > Q-BRIDGE-MIB.py
-build-pysnmp-mib CISCO-QOS-PIB-MIB.my > CISCO-QOS-PIB-MIB.py
-build-pysnmp-mib RFC-1215.my > RFC-1215.py
-build-pysnmp-mib SNMPv2-TC-v1.my > SNMPv2-TC-v1.py
-build-pysnmp-mib CISCO-RTTMON-TC-MIB.my > CISCO-RTTMON-TC-MIB.py
-build-pysnmp-mib CISCO-ETHER-CFM-MIB.my > CISCO-ETHER-CFM-MIB.py
-build-pysnmp-mib CISCO-RTTMON-MIB.my > CISCO-RTTMON-MIB.py
+for x in SNMPv2-SMI SNMPv2-TC IANAifType-MIB SNMPv2-CONF RFC1155-SMI RFC-1212 SNMPv2-MIB RFC1213-MIB IF-MIB RFC1271-MIB BRIDGE-MIB TOKEN-RING-RMON-MIB RMON-MIB RMON2-MIB SNMP-FRAMEWORK-MIB P-BRIDGE-MIB CISCO-SMI Q-BRIDGE-MIB CISCO-QOS-PIB-MIB RFC-1215 SNMPv2-TC-v1 CISCO-RTTMON-TC-MIB CISCO-ETHER-CFM-MIB CISCO-RTTMON-MIB ; do
+    echo ============== Working on $x  ==============
+    build-pysnmp-mib $x.my > $x.py
+    echo ============== Done ==============
+
+done
